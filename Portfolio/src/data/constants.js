095016d85a3c1f6edf94269d85fd6e914bcde81b
Ashu Singh <138131976+ashu1706@users.noreply.github.com>
2025-01-16 20:31:58 +0530

    Create constant.js

---
+++ Portfolio/src/data/constants.js
@@ -0,0 +1,375 @@
+export const Bio = {
+    name: "Ashu Singh",
+    roles: [
+      "Full Stack Developer",
+      "Machine Learning Engineer",
+      "UI/UX Designer",
+      "Programmer",
+    ],
+    description:
+      "I'm deeply passionate about exploring the vast realms of technology, with a particular interest in machine learning, computer vision, and AI-powered solutions. I enjoy diving into challenging problems, transforming innovative ideas into functional prototypes, and pushing the boundaries of what's possible with technology.",
+    github: "https://github.com/ashu1706",
+    resume:
+      "https://drive.google.com/file/d/1wVocZJPC1xqymv-KmLeFLb4oXXYf63db/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/ashu-singh-a49307262/",
+    leetcode: "https://leetcode.com/u/enashusingh001/",
+    email: "singhashu3102@gmail.com",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Django",
+          image: "https://datascientest.com/en/files/2023/05/django1.jpg",
+        },
+        {
+            name: "Flask",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1280px-Flask_logo.svg.png",
+          },
+        {
+          name: "SQL",
+          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY96BPkBqp2Z_gQdUVHpNh38YPdgP28F00yg&s",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Postgresql",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+      ],
+    },
+    {
+      title: "Machine Learning",
+      skills: [
+        {
+          name: "TensorFlow",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGOYXGizfi4kGTV5dpLRgJwHh-YcY18pEYlw&s",
+        },
+        {
+          name: "Pytorch",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoBwo70v-oZ0qGHyoUni1RLYCWg4REUz7znw&s",
+        },
+        {
+          name: "Keras",
+          image:
+            "https://victorzhou.com/static/c309c4c6a7bbdb43cf1f290786ce47ab/39600/keras-logo.png",
+        },
+        {
+          name: "Scikit-learn",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU6yrOw84wR4bNZNJWdKrY6VDMfn_FrAOBJw&s",
+        },
+        {
+            name: "OpenCV",
+            image:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1cV6JpoCif27ekKZfeQVOkdLFZBEyNes4cw&s",
+          },
+          {
+            name: "Numpy",
+            image:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRC2C9EVtvZjW_wQ3f9bEP2Fgla230C3kVYQ&s",
+          },
+          {
+            name: "Pandas",
+            image:
+              "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+          },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQlNcQUZyQR5rJ4gHKUUuvWw_68oqmXdz89w&s",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Docker",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        },
+        {
+          name: "Kubernetes",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR99vkXlOXicRs8PtMeSG4KiyWpULvHdG2QUA&s",
+        },
+        {
+          name: "PowerBI",
+          image:
+            "https://media.datacamp.com/legacy/v1724169856/image_ff55d03003.png",
+        },
+        {
+        name: "AWS",
+        image:
+          "https://miro.medium.com/v2/resize:fit:500/1*2STZmGfuaBZEACgUTTJzeg.png",
+      },
+        {
+          name: "Figma",
+          image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS62LVhts8-ERiNmlrfHQShVXasUYH38KKSRw&s",
+        },
+        {
+            name: "Netlify",
+            image:
+              "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+          },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://media.licdn.com/dms/image/v2/C510BAQExe5Cr2XEgmQ/company-logo_200_200/company-logo_200_200/0/1630565934368/testaing_logo?e=2147483647&v=beta&t=-s884AAxQGtTXIbjW_4voEwv8EJl6tf-XS5S-qRypSY",
+      role: "Machine Learning Engineer Intern",
+      company: "TestAIng Solutions Pvt. Ltd.",
+      date: "Jun 2024 - Aug 2023",
+      desc: "Collaborated with Tai Dev and Testing Team to integrate an ensemble model (Wav2Vec2, WavLM, Siamese network) achieving 99% accuracy in audio classification. Designed interactive dashboards with Streamlit and Power BI, enhancing usability by 95%, and led seamless production server deployment.",
+      skills: [
+        "CNN",
+        "Tensorflow",
+        "Scikit-learn",
+        "Librosa",
+        "Flask",
+        "Transformers",
+        "Numpy",
+        "Wav2Vec",
+        "Matplotlib",
+      ],
+    },
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZqq0SUMxJUrbsRjE6AET_tBH7NzMB2iioDQ&s",
+      role: "Research Intern",
+      company: "Indian Institute of Technology, Bhilai",
+      date: "June 2024 - July 2024",
+      desc: "Developed a deep learning framework for road anomaly detection using semantic segmentation, uncertainty estimation, and dissimilarity detection. Optimized model parameters through data analysis, improving detection accuracy by 10% across various road surfaces.",
+      skills: [
+        "R-CNN",
+        "YOLOv8",
+        "OpenCV",
+        "Object detection",
+        "Semantic segmentation",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://www.nitap.ac.in/assets/logo_big.jpg",
+      school: "National Institute of Technology, Arunachal Pradesh",
+      date: "Nov 2022 - Jun 2026",
+      grade: "9.11 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at National Institute of Technology, Arunachal Pradesh. I have completed 5 semesters and have a CGPA of 9.11. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjXyCppyAOMP8QW6ZZshKi70PrvbQbaLt8Jg&s",
+      school: "Sant Atulanand Convent School, Varanasi",
+      date: "Apr 2020 - Apr 2022",
+      grade: "92.4%",
+      desc: "I completed my class 12 high school education at Sant Atulanand Convent School, Varanasi, where I studied Science with Legal Studies.",
+      degree: "AISSCE(XII), Science with Legal Studies",
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjXyCppyAOMP8QW6ZZshKi70PrvbQbaLt8Jg&s",
+      school: "Sant Atulanand Convent School, Varanasi",
+      date: "Apr 2018 - Apr 2020",
+      grade: "98%",
+      desc: "I completed my class 10 education at Sant Atulanand Convent School, Varanasi.",
+      degree: "AISSE(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Blog-Post",
+      date: "Nov 2024 - Nov 2024",
+      description:
+      "Developed a user-friendly blogging platform designed to empower individuals to share their thoughts, stories, and expertise effortlessly. The platform features an intuitive interface for creating, editing, and publishing blogs, along with robust categorization and search functionality for easy content discovery",
+      image:
+        "https://github.com/ashu1706/Blog_post/blob/main/Screenshot%202024-12-29%20235140.png?raw=true",
+      tags: [
+        "Django",
+        "React Js",
+        "JavaScript",
+        "Sqlite3",
+      ],
+      category: "web app",
+      github: "https://github.com/ashu1706/Blog_post",
+      
+    },
+    {
+      id: 0,
+      title: "Celebrity-Image-Classification",
+      date: "Nov 2024 - Nov 2024",
+      description:
+      "Designed a powerful image classification application featuring a seamless drag-and-drop image upload interface powered by Dropzone, enabling users to classify images with ease. Leveraged a robust Support Vector Machine (SVM) model to achieve an impressive 87% classification accuracy. The application is supported by a secure Flask backend, ensuring reliable handling of image uploads and classification requests while maintaining data integrity. Through iterative updates and extensive usability testing, user satisfaction was enhanced by 90%, delivering a highly intuitive and efficient experience tailored to user needs.",
+      image:
+        "https://github.com/ashu1706/celebrity_image_classification/blob/main/ui_snapshot.jpg?raw=true",
+      tags: ["React Js", "Flask", "Python", "Scikit-learn", "Numpy", "OpenCV", "Support Vector Machine"],
+      category: "Machine Learning",
+      github: "https://github.com/ashu1706/celebrity_image_classification",
+    },
+    {
+      id: 1,
+      title: "Roomly",
+      date: "Oct 2024 - Nov 2024",
+      description:
+      "Developed a dynamic chat application that allows users to create and join chat rooms seamlessly on a global scale. The application ensures a smooth and interactive user experience, facilitating instant and reliable messaging across diverse chat rooms.",
+      image:
+        "https://github.com/ashu1706/Roomly/blob/main/Screenshot%202024-12-24%20150637.png?raw=true",
+      tags: [
+        "React Js",
+        "Sqlite3",
+        "Django",
+        "Python",
+        "WebSocket",
+        "Rest API",
+      ],
+      category: "web app",
+      github: "https://github.com/ashu1706/Roomly",
+    },
+    {
+      id: 2,
+      title: "Brain Tumor Detection",
+      date: "May 2024 - May 2024",
+      description:
+        "Developed a deep learning CNN model for binary classification to detect brain tumors in MRI images with an impressive accuracy of 99.2%. Preprocessed and augmented the dataset to enhance model performance and created an API using Python. Designed a React-based web app for the front end, delivering an intuitive interface for efficient tumor detection.",
+      image:
+        "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
+      tags: ["Python", "Keras", "TensorFlow", "Pickle", "React"],
+      category: "machine learning",
+      github: "https://github.com/karanakshat05/brain_tumour",
+      member: [
+        {
+          name: "Kumar Akshat",
+          img: "https://avatars.githubusercontent.com/u/142332675?v=4",
+          linkedin: "https://www.linkedin.com/in/kumar-akshat-9646891b3/",
+          github: "https://github.com/karanakshat05",
+        },
+        {
+          name: "Ashu Singh",
+          img: "https://avatars.githubusercontent.com/u/138131976?v=4",
+          linkedin: "https://www.linkedin.com/in/ashu-singh-a49307262/",
+          github: "https://github.com/ashu1706",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Weather App",
+      date: "Dec 2023 - Jan 2024",
+      description:
+        "A lightweight Weather API project that fetches and displays real-time weather data for any location. Built with Python and Flask, it provides a clean interface and seamless integration with popular weather API providers like OpenWeatherMap.  ",
+      image:
+      "https://github.com/ashu1706/Weather_API/blob/main/Screenshot%202024-12-30%20131943.png?raw=true",
+      tags: ["React", "JavaScript", "BootStrap", "OpenWeatherMap API"],
+      category: "web app",
+      github: "https://github.com/ashu1706/Weather_API",
+      webapp: "https://weatherapp-800.netlify.app/",
+    },
+    {
+      id: 10,
+      title: "Tic Tac Toe",
+      date: "Jan 2024 - Jan 2024",
+      description:
+        "Created a classic Tic Tac Toe game using JavaScript, featuring an interactive and responsive design for seamless gameplay.",
+      image:
+        "https://github.com/ashu1706/Tic_Tac_Toe/blob/main/Screenshot%202024-12-28%20183041.png?raw=true",
+      tags: ["JavaScript", "HTML", "CSS"],
+      category: "web app",
+      github: "https://github.com/ashu1706/Tic_Tac_Toe",
+      webapp: "https://tic-tac-toe-9be1f0.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Wildlife Surveillance",
+      date: "Jun 2024 - Present",
+      description:
+      "Developing a drone-based system integrating AI and IoT for real-time animal detection and event tagging, addressing scenarios like animal-human conflicts and herd tracking. Designing a comprehensive dashboard for real-time monitoring and actionable insights, boosting efficiency and reducing response time through an advanced alert system.",
+      image:
+        "https://github.com/karanakshat05/wildlife_monitoring/blob/main/Screenshot%202024-12-30%20000330.png?raw=true",
+      tags: ["CNN", "R-CNN", "OpenCV", "TensorFlow", "Matplotlib", "Nvidia Jetson", "IOT"],
+      category: "machine learning",
+      github: "https://github.com/karanakshat05/wildlife_monitoring",
+      member: [
+        {
+          name: "Kumar Akshat",
+          img: "https://avatars.githubusercontent.com/u/142332675?v=4",
+          linkedin: "https://www.linkedin.com/in/kumar-akshat-9646891b3/",
+          github: "https://github.com/karanakshat05",
+        },
+        {
+          name: "Ashu Singh",
+          img: "https://avatars.githubusercontent.com/u/138131976?v=4",
+          linkedin: "https://www.linkedin.com/in/ashu-singh-a49307262/",
+          github: "https://github.com/ashu1706",
+        },
+      ],
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
